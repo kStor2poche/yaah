4e3f6d82e8c33d4e1ef21ed4e7cef4fcca2c3ed7
kStor2poche <leo.lagarrigue@orange.fr>
2023-02-20 21:07:34 +0100

    Added search option to help script and to README.md

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ A temporary side project of mine made to offer a simple command-line helper for 
   
 General usage is `yaah [switch] [packages]`, where `[switch]` can be one of the following options :
 - `-s` for installing packages  
+- `-ss` for searching packages
 - `-r` for removing packages (currently not implemented)  
 - `-u` for updating the packages found in `$GITPATH`  
 - `-g` for modifying the `GITPATH` variable, i.e. the path where the aur package folders are stored  
--- yaah-help.sh
+++ yaah-help.sh
@@ -7,6 +7,7 @@ if [[ -z $1 ]];then
     echo -e "\e[3mYAAH - YET ANOTHER AUR HELPER\e[0m\n"
     echo -e "Usage : yaah [ -u | -S[s] | -r[s] | -g[e] ] [package names]\n"
     echo "Switches : -s -> install packages
+           -ss -> search for packages
            -u -> update packages and install the ones provided in the rest of the command
            -r -> remove packages
            -g -> change the \$GITPATH variable, which contains the path to the package installation folders
